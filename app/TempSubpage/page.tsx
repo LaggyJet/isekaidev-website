f0a2d112b42b27bacdf6b849113569c118cdcd62
LaggyJet <jskrillex6@gmail.com>
2025-10-08 02:20:33 -0400

    Init setup

---
+++ app/TempSubpage/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Something() {
+    return (
+        <div>
+            <h1>This is the subpage</h1>
+            <a href="/">Back to home</a>
+        </div>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to IsekaiDev</h1>
+            <p>This is the homepage.</p>
+            <a href="/TempSubpage">Go to subpage</a>
+        </div>
+    );
+}
